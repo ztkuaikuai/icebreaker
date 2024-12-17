0798d50aba6f45ebe0f4f3c01a8eaaade646b899
ztkuaikuai <2647731903@qq.com>
2024-12-17 17:44:30 +0800

    fix: 修复水合错误的问题

--- hooks/useData.tsx
+++ hooks/useData.tsx
@@ -17,33 +17,34 @@ const setLocalStorage = (Key: string, Value: unknown): void => {
     localStorage.setItem(Key, JSON.stringify(Value))
 }
 
-const getRandomIndex = (data: Data[], history: Record<Data["id"], Data>): number => {
-    let index: number
-    do {
-        index = Math.floor(Math.random() * data.length)
-    } while (history[index] !== undefined)
-    return index
+const getRandomIndex = (data: Data[], curIndex: number | null): number => {
+    let nextIndex: number = Math.floor(Math.random() * data.length)
+    while (nextIndex === curIndex) {
+        nextIndex = Math.floor(Math.random() * data.length)
+    } 
+    return nextIndex
 }
 
 export const useData = () => {
-    // 水合错误：服务端渲染的数据和客户端初始渲染的数据不一致导致
-    const [index, setIndex] = useState(0)
+    const [index, setIndex] = useState<number | null>(null)
     const [history, setHistory] = useState<Record<Data["id"], Data>>({})
-    
+
     useEffect(() => {
         const storedHistory = getLocalStorage(HISTORY_KEY) as Record<Data["id"], Data> | null
         if (storedHistory !== null) {
             setHistory(storedHistory)
         }
+        const initialIndex = getRandomIndex(data, index)
+        setIndex(initialIndex)
     }, [])
 
     const switchData = (): void => {
-        const newIndex = getRandomIndex(data, history)
+        const newIndex = getRandomIndex(data, index)
         setIndex(newIndex)
         const newHistory = { ...history, [newIndex]: data[newIndex] }
         setHistory(newHistory)
         setLocalStorage(HISTORY_KEY, newHistory)
     }
 
-    return [data[index]['text'], switchData] as const
+    return [index === null ? '' : data[index]['text'], switchData] as const
 }
